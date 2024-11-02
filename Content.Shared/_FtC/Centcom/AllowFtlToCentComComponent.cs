0b6fc31e4fe8b86cce368e61b095fcfc7a680b4a
Walkman1961 <w3270050@gmail.com>
2024-11-02 16:24:51 +0300

    AllowSpawnOnCentcomm

---
+++ Content.Shared/_FtC/Centcom/AllowFtlToCentComComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._FtC.Arrivals;
+
+[RegisterComponent,NetworkedComponent]
+public sealed partial class AllowFtlToCentComComponent : Component
+{
+
+}
